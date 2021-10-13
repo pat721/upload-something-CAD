05d5ead9e3e21054fe0dc6781eb1ee71e16a0650
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2021-10-13 15:18:02 +0200

    Cleanup

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
-const fs = require('fs')
-const { PrismaClient } = require('@prisma/client')
 const uploadsController = require('../controllers/uploads')
 
-const prisma = new PrismaClient()
 var router = express.Router()
-
-const uploadDirectory = __dirname + '/../public/user_uploads/';
 
 router.use(fileUpload());
 
@@ -15,9 +10,6 @@ router.post('/uploads', uploadsController.newUpload);
 router.get('/uploads/:uploadFileName', uploadsController.getSingleUpload);
 router.get('/uploads', uploadsController.getAllUploads);
 router.delete('/uploads/delete/:uploadFileName', uploadsController.deleteSingleUpload);
-
-
-
 
 module.exports = router
 
